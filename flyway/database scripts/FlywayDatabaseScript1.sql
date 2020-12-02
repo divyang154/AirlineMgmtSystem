8aa9efa291b4b3f125410a3dffba88bc7dd25a0e
Divyang Acharya <acharyadivyang789@gmail.com>
2020-12-02 22:07:56 +0530

    script

--- flyway/database scripts/FlywayDatabaseScript1.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: select * from airline_tb
-LIMIT 0, 10
-
--- Date: 2020-12-02 22:02
-*/
-INSERT INTO `airline_tb` (`airlineId`,`name`,`createdOn`,`updatedOn`,`createdBy`,`updatedBy`) VALUES (17,'Indigo','2020-12-01 20:47:21','2020-12-01 20:47:21',1,1);
